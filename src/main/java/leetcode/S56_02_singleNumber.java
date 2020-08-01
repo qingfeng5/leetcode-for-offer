a3a5ec455d1e631a0671218f08815a95bd651155
qingfeng <wq_qingfeng@163.com>
2020-08-01 11:57:14 +0800

    gengxin

---
+++ src/main/java/leetcode/S56_02_singleNumber.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * Created by 清风
+ * 2020/8/1 9:41
+ *
+ * 剑指 Offer 56 - II. 数组中数字出现的次数 II
+ * 在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [3,4,3,3]
+ * 输出：4
+ * 示例 2：
+ *
+ * 输入：nums = [9,1,7,9,7,9,7]
+ * 输出：1
+ */
+public class S56_02_singleNumber {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int num : nums){
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+
+}
---
+++ src/main/java/leetcode_basic/S1_twoSum.java
@@ -0,0 +1,42 @@
+package leetcode_basic;
+
+/**
+ * Created by 清风
+ * 2020/7/26 17:37
+ * 1. 两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class S1_twoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        if (nums.length < 2){
+            throw new IllegalArgumentException("Illegal argument numbers");
+        }
+        int l=0;
+        int r=nums.length-1;
+        while (l < r){
+            if ( nums[l] + nums[r] == target){
+                int[] res ={l , r};
+                return res;
+            }
+            else if (l<r && nums[l] + nums[r] <target ){
+                l++;
+            }
+            else (nums[l] + nums[r] > target) {
+                r--;
+            }
+
+        }
+        throw new IllegalArgumentException("Illegal argument numbers");
+
+    }
+}
